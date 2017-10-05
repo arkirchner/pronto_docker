326fed9133e228429d2bc38f91b493c8f805de54
Armin Kirchner <post.armin@gmail.com>
2017-10-05 12:35:53 +0900

    Added ESLINT airbnb ES5 settings

--- setup_eslint.sh
+++ setup_eslint.sh
@@ -1,3 +1,11 @@
+# airbnb-base for ES6
+(
+  export PKG=eslint-config-airbnb-es5;
+  npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --global "$PKG@latest"
+)
+npm install -g eslint-plugin-react babel-eslint
+
+# airbnb-base for ES6
 (
   export PKG=eslint-config-airbnb-base;
   npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --global "$PKG@latest"
