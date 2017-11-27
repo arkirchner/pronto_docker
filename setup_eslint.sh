0490c571ba425a1f06d0bf1663aea277a32f5123
Armin Kirchner <post.armin@gmail.com>
2017-11-27 14:49:05 +0900

    Added eslint for jc portal

--- setup_eslint.sh
+++ setup_eslint.sh
@@ -1,12 +1,19 @@
-# airbnb-base for ES6
+npm install -g eslint eslint-plugin-react babel-eslint
+
+# airbnb-base for ES5
 (
   export PKG=eslint-config-airbnb-es5;
   npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --global "$PKG@latest"
 )
-npm install -g eslint-plugin-react babel-eslint
 
 # airbnb-base for ES6
 (
   export PKG=eslint-config-airbnb-base;
+  npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --global "$PKG@latest"
+)
+
+# aribnb for ES6
+(
+  export PKG=eslint-config-airbnb;
   npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --global "$PKG@latest"
 )
